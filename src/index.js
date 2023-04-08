8f893564e5354e2960016688e7b336d398748e7d
Vonanji Kasambala <Vonanji.Kasambala@jda.com>
2023-04-08 13:48:08 +0200

    add all my files

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ window.fbAsyncInit = function () {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Game />
+    <App />
   </React.StrictMode>
 );
 
